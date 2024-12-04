09a1c6516f9b84e49e0ff48e051ba00400ceb0fc
Gustavo <ghenriquesouza1991@gmail.com>
2024-12-04 19:28:07 -0300

    Finalizado responsivo

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -21,12 +21,6 @@ export default function Doados(){
                     <p>Ficção</p>
              </article>
 
-             <article>
-                    <img src={livro} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-             </article>
             </section>
         </section>
     )
